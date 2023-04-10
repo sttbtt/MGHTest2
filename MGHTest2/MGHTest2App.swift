b7e9a987d4e430c6d7e3562b1383eef9540d64da
Scott Bennett <stt.btt@gmail.com>
2023-04-10 14:07:13 -0400

    Initial Commit

---
+++ MGHTest2/MGHTest2App.swift
@@ -0,0 +1,17 @@
+//
+//  MGHTest2App.swift
+//  MGHTest2
+//
+//  Created by Scott Bennett on 4/10/23.
+//
+
+import SwiftUI
+
+@main
+struct MGHTest2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
